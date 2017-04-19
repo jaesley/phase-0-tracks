9bd160fbfa612d8023d17d5603a120606545dd57
Jae Easley <jaesley@gmail.com>
2017-04-19 11:20:06 -0700

    Improve wording

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,34 +1,41 @@
-+puts "What is the hamster's name?"
- +  name = gets.chomp
- +
- +puts "What is your volume tolerance level? (1 = lowest volume, 10 = highest volume)"
- +  volume = gets.chomp
- +  volume = volume.to_i
- +  until volume >= 1 && volume <= 10
- +    puts "What is your volume tolerance level? (1 = lowest volume, 10 = highest volume)"
- +  volume = gets.chomp
- +    volume = volume.to_i
- +end
- +
- +puts "What color is the hamster's fur?"
- +  furcolor = gets.chomp
- +
- +puts "Is the hamster a good candidate for adoption? (y/n)"
- +  adoption = gets.chomp
- +  until adoption == "y" || adoption == "n"
- +    puts "Is the hamster a good candidate for adoption? (y/n)"
- +    adoption = gets.chomp
- +  end
- +
- +puts "What is the hamster's approximate age?"
- +  age = gets.chomp
- +  age = age.to_i
- +  if age == ""
- +  age = nil
- +end
- +
- +puts "Your hamster's name is #{name}."
- +puts "Your voume tolerance level is #{volume}."
- +puts "#{name}'s fur color is #{furcolor}."
- +puts "Adoption approval status: #{adoption}"
- +puts "#{name}'s approximate age is #{age}" 
+puts "What is the hamster's name?"
+  name = gets.chomp
+ 
+puts "What is the hamster's volume level? (1 = lowest volume, 10 = highest volume)"
+  volume = gets.chomp
+  volume = volume.to_i
+ 
+  until volume >= 1 && volume <= 10
+    puts "What is your volume tolerance level? (1 = lowest volume, 10 = highest volume)"
+ 	  volume = gets.chomp
+      volume = volume.to_i
+    end
+ 
+puts "What color is the hamster's fur?"
+  furcolor = gets.chomp
+ 
+puts "Is the hamster a good candidate for adoption? (y/n)"
+  adoption = gets.chomp
+   
+   until adoption == "y" || adoption == "n"
+    puts "Is the hamster a good candidate for adoption? (y/n)"
+    adoption = gets.chomp
+  end
+ 
+puts "What is the hamster's approximate age?"
+  age = gets.chomp
+    if age == ""
+     	age = nil
+    else 
+    	age.to_i
+    end
+
+puts ""
+puts "Hamster's name is #{name}."
+puts "Hamster's volume level is #{volume}."
+puts "Hamster's fur color is #{furcolor}."
+	if adoption == true
+		puts "Hamster's adoption approval status is approved."
+	else 
+		puts "Hamster's adoption approval status is not approved."
+puts "Hamster's approximate age is #{age}" 
