f9de9c11575cc1e5a2aaaacd638c8bdde2261875
Jae Easley <jaesley@gmail.com>
2017-04-20 11:21:30 -0700

    Add basic input validation for y/n

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,10 +10,20 @@ puts "What year were you born?"
 
 birthyear = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 
 garlic = gets.chomp
 
-puts "Would you like to enroll i nthe company's health insurance?"
+until garlic = "y" || garlic = "n"
+	puts "Please use 'y' or 'n'. Now, should we order some garlic bread?"
+	garlic = gets.chomp
+end
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 
 insurance = gets.chomp
+
+until insurance = "y" || insurance = "n"
+	puts "Please use 'y' or 'n'. Now, would you like to enroll?"
+	insurance = gets.chomp
+end
