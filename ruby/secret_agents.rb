ea4ee65e9c0b4c8fa422aed8628f505ac08b7d0e
Jae Easley <jaesley@gmail.com>
2017-04-22 12:30:44 -0700

    Clarify pseudocode and add input error checking

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,68 +1,111 @@
 # OUR METHOD DECLARATIONS
 
-# Encrypt
-# Store alphabet variable
+# 1. Encrypt
+
 def encrypt(password)
-# Loop through each letter of the password by index.
+
+# Store starting point index, alphabet, and blank encrypted variable
+
   index = 0
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   encrypted = ""
+
+# Loop through each letter of the password starting at index.
+
   while index < password.length
+
 # If the character is a space, skip it
 
-# Otherwise advance that letter via index + 1
-	  letter = password[index]
-	  newindex = alphabet.index(letter)
-# If the index + 1 is greater than 25 reset it back to 0
-if newindex + 1 == alphabet.length
-	      newindex = -1
+# Otherwise store newindex as the alphabetical index of the current password letter
+
+		newindex = alphabet.index(password[index])
+
+# If the newindex + 1 equals the alphabet length reset index back to -1
+# so 'z' will become 'a'
+
+			if newindex + 1 == alphabet.length
+				newindex = -1
 	    end
-	  newletter = alphabet[newindex + 1]
-    encrypted[index] = newletter
+
+# Set encrypted[index] to the next letter of the alphabet
+
+    encrypted[index] = alphabet[newindex + 1]
+
+# Raise index by 1 and start next iteration
+
 	  index += 1
-# Store the next letter in the variable
-  end
+	end
+
+# After all letters have been adjusted, return encrypted
+  
   return encrypted
 end
 
-# puts encrypt(password)
-
 # Decrypt
-# Get the index of each letter of encrypted string
+
 def decrypt(password)
-# Store letter at index - 1 from the alphabet
+
+# Store starting point index, alphabet, and blank encrypted variable
+
   index = 0
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   decrypted = ""
+
+# Loop through each letter of the password starting at index.
+
   while index < password.length
-    letter = password[index]
-    newindex = alphabet.index(letter)
+
+# If the character is a space, skip it
+
+# Otherwise store newindex as the alphabetical index of the current password letter
+
+    newindex = alphabet.index(password[index])
+
+# If the newindex - 1 equals -1 reset index back to alphabet length
+# so 'a' will become 'z'
       if newindex - 1 == -1
         newindex = alphabet.length
       end
-    newletter = alphabet[newindex -1]
-    decrypted[index] = newletter
+
+# Set decrypted[index] to the previous letter of the alphabet
+
+    decrypted[index] = alphabet[newindex - 1]
+
+# Raise index by 1 and start next iteration
+
     index += 1
   end
+
+# After all letters have been adjusted, return encrypted
+
   return decrypted
 end
 
-# puts decrypt(password)
-
 # If you decrypt the encryption, you'll get the original back!
 # puts decrypt(encrypt("swordfish"))
+# returns:
+# "swordfish"
 
 # OUR DRIVER CODE
 
 # Asks a secret agent (the user) whether they would like to decrypt or encrypt a password
+
 puts "Would you like to decrypt or encrypt a password?"
+method = gets.chomp.downcase
 
-method = gets.chomp
+# Prompts again if input is not 'encrypt' or 'decrypt'.
+
+until method == "encrypt" || method == "decrypt"
+	puts "I didn't understand that. Would you like to decrypt or encrypt a password?"
+	method = gets.chomp.downcase
+end
+
 # Asks them for the password
-puts "What is your password?"
 
+puts "What is your password?"
 password = gets.chomp
-# Conducts the requested operation, prints the result to the screen, and exits
+
+# Prints the requeseted operation's result to the screen
 
 if method == "encrypt"
   puts encrypt(password)
