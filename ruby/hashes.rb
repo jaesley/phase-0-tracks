1139f48791641765a3c7a2463328a5ebe383e5a6
Jae Easley <jaesley@gmail.com>
2017-04-24 11:29:11 -0700

    Move type validation to end

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -23,12 +23,12 @@ client[:name] = gets.chomp
 # Get client's age and convert to integer.
 
 puts "What is the client's age?"
-client[:age] = gets.chomp.to_i
+client[:age] = gets.chomp
 
 # Get client's number of children and convert to integer.
 
 puts "What is the client's number of children?"
-client[:children] = gets.chomp.to_i
+client[:children] = gets.chomp
 
 # Get client's decor theme.
 
@@ -38,7 +38,7 @@ client[:theme] = gets.chomp
 # Get client's budget and convert to integer.
 
 puts "What is the client's budget?"
-client[:budget] = gets.chomp.to_i
+client[:budget] = gets.chomp
 
 # Check client's approval status.
 
@@ -50,14 +50,6 @@ client[:approval] = gets.chomp
 until client[:approval] == "y" || client[:approval] == "n"
   puts "Please enter 'y' or 'n'. Did the client approve the design sketches?"
   client[:approval] = gets.chomp
-end
-
-# Convert to boolean.
-
-if client[:approval] == "y"
-  client[:approval] = true
-else 
-  client[:approval] = false
 end
 
 # Check if design will be featured on HGTV.
@@ -72,21 +64,13 @@ until client[:hgtv] == "y" || client[:hgtv] == "n"
   client[:hgtv] = gets.chomp
 end
 
-# Convert to boolean.
-
-if client[:hgtv] == "y"
-  client[:hgtv] = true
-else 
-  client[:hgtv] = false
-end
-
 # Print client info.
 
 puts client
 
 # Check if key needs updating.
 
-puts "Which key should be updated? If all keys are correct, enter 'none'."
+puts "Which key should be updated? If all keys are correct, enter 'none' to finalize your form."
 key = gets.chomp
 
 # If not 'none', get corrected key value.
@@ -96,17 +80,25 @@ if key != "none"
   client[key.to_sym] = gets.chomp
 end
 
-# Reconvert data type if necessary.
-if key = "age" || key == "children" || key == "budget"
-  client[key.to_sym] = client[key.to_sym].to_i
+# Convert data types for finalized form.
+
+client[:age] = client[:age].to_i
+client[:children] = client[:children].to_i
+client[:budget] = client[:budget].to_i
+
+if client[:approval] == "y"
+  client[:approval] = true
+else 
+  client[:approval] = false
 end
 
-if ( key == "hgtv" || key == "approval" ) && client[key.to_sym] == "y"
-  client[key.to_sym] = true
+if client[:hgtv] == "y"
+  client[:hgtv] = true
+else 
+  client[:hgtv] = false
 end
 
-if ( key == "hgtv" || key == "approval" ) && client[key.to_sym] == "n"
-  client[key.to_sym] = false
-end
+# Print finalized form.
 
+puts "Your finalized form is ready!"
 puts client
