335cfce84904f9c3eeeb621450136561c88a741e
Jae Easley <jaesley@gmail.com>
2017-04-24 10:36:59 -0700

    Update pseudocode and data types

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -16,28 +16,33 @@ client = {
 
 # Update user-entered variables for hash.
 
+# Get client's name.
 puts "What is the client's name?"
-
 client[:name] = gets.chomp
 
-puts "What is the client's age?"
+# Get client's age and convert to integer.
 
-client[:age] = gets.chomp
+puts "What is the client's age?"
+client[:age] = gets.chomp.to_i
+
+# Get client's number of children and convert to integer.
 
 puts "What is the client's number of children?"
+client[:children] = gets.chomp.to_i
 
-client[:children] = gets.chomp
+# Get client's decor theme.
 
 puts "What is the client's decor theme?"
-
 client[:theme] = gets.chomp
 
-puts "What is the client's budget?"
+# Get client's budget and convert to integer.
 
-client[:budget] = gets.chomp
+puts "What is the client's budget?"
+client[:budget] = gets.chomp.to_i
+
+# Check client's approval status.
 
 puts "Did the client approve the design sketches? (y/n)"
-
 client[:approval] = gets.chomp
 
 # Force y/n answer.
@@ -50,28 +55,31 @@ end
 # Convert to boolean.
 
 if client[:approval] == "y"
-  client[:approval] == true
+  client[:approval] = true
 else 
-  client[:approval] == false
+  client[:approval] = false
 end
 
-puts "Is this redecoration going to be on HGTV? (y/n)"
+# Check if design will be featured on HGTV.
 
+puts "Is this redecoration going to be on HGTV? (y/n)"
 client[:hgtv] = gets.chomp
 
 # Force y/n answer.
 
 until client[:hgtv] == "y" || client[:hgtv] == "n"
   puts "Please enter 'y' or 'n'. Is this redecoration going to be on HGTV?"
-  client[:approval] = gets.chomp
+  client[:hgtv] = gets.chomp
 end
 
 # Convert to boolean.
 
 if client[:hgtv] == "y"
-  client[:hgtv] == true
+  client[:hgtv] = true
 else 
-  client[:hgtv] == false
+  client[:hgtv] = false
 end
+
+# Print client info.
 
 puts client
