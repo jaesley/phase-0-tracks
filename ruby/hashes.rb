b1740242d1d295d5dcddeecf0af668336d47944c
Jae Easley <jaesley@gmail.com>
2017-04-24 10:18:47 -0700

    Force booleans for y/n questions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,12 +3,12 @@
 # Set up blank hash.
 
 client = {
-  :name => ""
-  :age => ""
-  :children => ""
-  :theme => ""
-  :budget => ""
-  :approval => ""
+  :name => "",
+  :age => "",
+  :children => "",
+  :theme => "",
+  :budget => "",
+  :approval => "",
   :hgtv => ""
 }
 
@@ -36,10 +36,42 @@ puts "What is the client's budget?"
 
 client[:budget] = gets.chomp
 
-puts "Did the client approve the design sketches?"
+puts "Did the client approve the design sketches? (y/n)"
 
 client[:approval] = gets.chomp
 
-puts "Is this redecoration going to be on HGTV?"
+# Force y/n answer.
+
+until client[:approval] == "y" || client[:approval] == "n"
+  puts "Please enter 'y' or 'n'. Did the client approve the design sketches?"
+  client[:approval] = gets.chomp
+end
+
+# Convert to boolean.
+
+if client[:approval] == "y"
+  client[:approval] == true
+else 
+  client[:approval] == false
+end
+
+puts "Is this redecoration going to be on HGTV? (y/n)"
 
 client[:hgtv] = gets.chomp
+
+# Force y/n answer.
+
+until client[:hgtv] == "y" || client[:hgtv] == "n"
+  puts "Please enter 'y' or 'n'. Is this redecoration going to be on HGTV?"
+  client[:approval] = gets.chomp
+end
+
+# Convert to boolean.
+
+if client[:hgtv] == "y"
+  client[:hgtv] == true
+else 
+  client[:hgtv] == false
+end
+
+puts client
