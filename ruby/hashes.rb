87be70b73213e5529b3880817b7ad6261b9c4eba
Jae Easley <jaesley@gmail.com>
2017-04-24 10:08:00 -0700

    Update with basic driver code

---
+++ ruby/hashes.rb
@@ -0,0 +1,32 @@
+# METHODS
+
+
+# DRIVER CODE
+
+puts "What is the client's name?"
+
+name = gets.chomp
+
+puts "What is the client's age?"
+
+age = gets.chomp
+
+puts "What is the client's number of children?"
+
+children = gets.chomp
+
+puts "What is the client's decor theme?"
+
+theme = gets.chomp
+
+puts "What is the client's budget?"
+
+budget = gets.chomp
+
+puts "Did the client approve the design sketches?"
+
+approval = gets.chomp
+
+puts "Is this redecoration going to be on HGTV?"
+
+hgtv = gets.chomp
