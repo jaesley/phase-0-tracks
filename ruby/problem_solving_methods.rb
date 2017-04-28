82fd3f5920ee853a2b8c6f3a1d826bfdc10f1e10
Jae Easley <jaesley@gmail.com>
2017-04-28 14:14:45 -0700

    Add bubblesort method during pair session

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -32,3 +32,36 @@ def fib(x)
 end
 
 fib(3)
+
+# Sort an array via bubble sort
+
+# method bubblesort(array)
+#   for array length - 1 times
+#     x = 0
+#     compare index x and index x + 1
+#       if index x > index x + 1
+#         swap index x and index x + 1
+#       end if
+#     increment x by 1
+#   end loop
+# end method
+
+def bubblesort(array)
+  loop do
+    sorted = false
+  
+    (array.length - 1).times do |x|
+      if array[x] > array[x + 1]
+        array[x], array[x + 1] = array[x + 1], array[x]
+        sorted = true
+      end
+    end
+    
+    break if not sorted
+  end
+  
+  return array
+end
+
+array1 = [5,3,56,2,4,32]
+bubblesort(array1)
