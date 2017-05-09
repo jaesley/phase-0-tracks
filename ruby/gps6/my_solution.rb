bfe884ad5edd207292577fb4c165e6d08fd6b797
Jae Easley <jaesley@gmail.com>
2017-05-08 21:39:53 -0700

    Add require_relative desc + method comments

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,18 +4,25 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Makes a link to a second related Ruby file
+# and virtually imports the file
+# require takes the full file path as opposed to 
+# require_relative which does not
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+  # Creates a new instance of the VirusPredictor class with instance variables
+  # passed as parameters to the .new call
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # Calls .predicted_deaths and .speed_of_spread methods and
+  # provides the output of those private methods
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
@@ -23,6 +30,8 @@ class VirusPredictor
 
   private
 
+  # Takes the state's data and multiplies population by a factor
+  # determined by the state's population density
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +50,8 @@ class VirusPredictor
 
   end
 
+  # Takes the state's data and determines the speed of spread
+  # based on a factor of population density
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
