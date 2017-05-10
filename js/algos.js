56688959519517543f816c04fd384cb7ed7fbd36
Jae Easley <jaesley@gmail.com>
2017-05-10 14:12:17 -0700

    Add pairMatch() function

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,5 @@
+// Find Longest Phrase
+
 // function findLongestWord(array)
 //   create a blank longestWord variable
 //   for every item in array, starting at index 0:
@@ -6,11 +8,12 @@
 //     else if current word is longer than longestWord
 //       set longestWord to current word
 //     end if
-//     return longestWord
+//   end loop
+//   return longestWord
 // end function
 
 function findLongestPhrase(array) {
-  longestWord = "";
+  var longestWord = "";
   for ( i = 0; i < array.length; i++ ) {
     if ( array[i].length > longestWord.length ) {
       longestWord = array[i];
@@ -19,9 +22,47 @@ function findLongestPhrase(array) {
   return longestWord;
 }
 
-array_test = ["long phrase", "longest phrase", "longer phrase"];
+var array_test = ["long phrase", "longest phrase", "longer phrase"];
 console.log(findLongestPhrase(array_test));
 
-array_pets = ["Blair", "Lylle", "Hallow", "Ewok", "Blunder", "Panda", "Puck"];
-console.log(findLongestPrase(array_pets));
+var array_pets = ["Blair", "Lylle", "Hallow", "Ewok", "Blunder", "Panda", "Puck"];
+console.log(findLongestPhrase(array_pets));
 
+// Data Entry
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+var steven = new Person("Steven", 54);
+var tamir = new Person("Tamir", 54);
+
+function Creature(animal, legs) {
+  this.animal = animal;
+  this.legs = legs;
+}
+
+var dog4 = new Creature("Dog", 4);
+var dog3 = new Creature("Dog", 3);
+
+// Find Matching Key/Value Pair
+
+// function pairMatch(object1, object2)
+//   for each key in object1, starting at 0
+//     if key matches corresponding key in object2
+//       return true
+//   end loop
+// end function(
+
+function pairMatch(object1, object2) {
+  for ( var k in object1 ) {
+    if ( object1[k] === object2[k] ) {
+      return true;
+    }
+  }
+  return false
+}
+
+console.log(pairMatch(steven, tamir));
+console.log(pairMatch(dog4, dog3))
