360140e698f1e16674db64685221477f54293f59
Jae Easley <jaesley@gmail.com>
2017-05-20 18:24:17 -0700

    File cleanup

--- databases/alt_tracker/alt_tracker.rb
+++ databases/alt_tracker/alt_tracker.rb
@@ -11,7 +11,6 @@
 #   seemed like a good basis for a schema to practice my SQL + Ruby skills on.
 
 require 'sqlite3'
-require_relative 'game_db'
 
 # create database
 # create tables: players, games, characters
@@ -59,19 +58,16 @@ db.execute(cmd_create_table_players)
 db.execute(cmd_create_table_games)
 db.execute(cmd_create_table_characters)
 
-# method add_player
-#   get name
-#   get location
-#   add to players table
-# end method
-
-
 def get_player_info(db)
   puts "Player's name:"
   name = gets.chomp
   puts "Player's location (state if American, country otherwise):"
   location = gets.chomp
   add_player(db, name, location)
+end
+
+def add_player(db, name, location)
+  db.execute("INSERT INTO players (name, location) VALUES (?, ?)", [name, location])
 end
 
 def get_game_info(db)
@@ -84,6 +80,10 @@ def get_game_info(db)
   puts "Game's current activity status (true/false):"
   activity_status = gets.chomp
   add_game(db, name, setting, genre, activity_status)
+end
+
+def add_game(db, name, setting, genre, activity_status)
+  db.execute("INSERT INTO games (name, setting, genre, activity_status) VALUES (?, ?, ?, ?)", [name, setting, genre, activity_status])
 end
 
 def get_character_info(db)
@@ -100,14 +100,6 @@ def get_character_info(db)
   add_character(db, name, template, game_id, player_id)
 end
 
-def add_player(db, name, location)
-  db.execute("INSERT INTO players (name, location) VALUES (?, ?)", [name, location])
-end
-
-def add_game(db, name, setting, genre, activity_status)
-  db.execute("INSERT INTO games (name, setting, genre, activity_status) VALUES (?, ?, ?, ?)", [name, setting, genre, activity_status])
-end
-
 def add_character(db, name, template, game_id, player_id)
   db.execute("INSERT INTO characters (name, template, game_id, player_id) VALUES (?, ?, ?, ?)", [name, template, game_id, player_id])
 end
@@ -122,23 +114,43 @@ end
 
 def view_options(db)
   options = {}
-  options[1] = ["View all players.", "view_players(db)"]
-  # options[2] = ["View all games.", view_games(db)]
-  # options[3] = ["View all characters.", view_characters(db)]
-  # options[5] = ["View all active games.", view_games_active(db)]
-  # options[6] = ["View all games in a genre", view_games_genre(db)]
-  # options[7] = ["View all characters played by a player.", view_characters_player(db)]
-  # options[8] = ["View all characters on a game.", view_characters_game(db)]
-  # options[9] = ["View all characters played by a player on a game.", view_characters_player_game(db)]
+  options[1] = "View all players."
+  options[2] = "View all games."
+  options[3] = "View all characters."
+  options[4] = "View all active games."
+  options[5] = "View all games in a genre"
+  options[6] = "View all characters played by a player."
+  options[7] = "View all characters on a game."
+  options[8] = "View all characters played by a player on a game."
 
   options.each do | index, option |
-    puts "#{index}. #{option[0]}"
+    puts "#{index}. #{option}"
   end
 
-  view = gets.chomp.to_i
-  options[view][1]
-  # It runs the cmd text when adding to hash. How to make it add it as text
-  # and then run the command by calling it later by hash?
+  id = gets.chomp.to_i
+
+  case id
+  when 1
+    view_players(db)
+  when 2
+    view_games(db)
+  when 3
+    view_characters(db)
+  when 4
+    view_games_active(db)
+  when 5
+    puts "What genre would you like to view?"
+    genre = gets.chomp
+    view_games_genre(db, genre)
+  when 6
+    view_characters_player
+  when 7
+    view_characters_game
+  when 8
+    view_characters_player_game
+  else
+    "Enter the number of the view you would like."
+  end
 end
 
 def view_players(db)
@@ -160,6 +172,58 @@ def view_characters(db)
   characters.each do |character|
     puts "#{character[0]}\t#{character[1]}\t#{character[2]}\t#{character[3]}\t#{character[4]}"
   end
+end
+
+def view_games_active(db)
+  games = db.execute("SELECT * FROM games WHERE activity_status = \"true\"")
+  games.each do |game|
+    puts "#{game['id']}\t#{game['name']}\t#{game['setting']}\t#{game['genre']}\t#{game['activity_status']}"
+  end
+end
+
+def view_games_genre(db, genre)
+  games = db.execute("SELECT * FROM games WHERE genre = \"#{genre}\"")
+  games.each do |game|
+    puts "#{game['id']}\t#{game['name']}\t#{game['setting']}\t#{game['genre']}\t#{game['activity_status']}"
+  end
+end
+
+def edit_player_info(db)
+  puts "Enter the number of the player you would like to edit."
+  view_players(db)
+  id = gets.chomp
+  puts "Would you like to update name or location?"
+  field = gets.chomp.downcase
+  puts "What is the updated #{field}?"
+  correction = gets.chomp
+  db.execute("UPDATE players SET #{field} = \"#{correction}\" WHERE id = #{id}")
+end
+
+def edit_game_info(db)
+  puts "Enter the number of the game you would like to edit."
+  view_games(db)
+  id = gets.chomp
+  puts "Would you like to update name, setting, genre, or activity_status?"
+  field = gets.chomp.downcase
+  puts "What is the updated #{field}?"
+  correction = gets.chomp
+  db.execute("UPDATE games SET #{field} = \"#{correction}\" WHERE id = #{id}")
+end
+
+def edit_character_info(db)
+  puts "Enter the number of the character you would like to edit."
+  view_characters(db)
+  id = gets.chomp
+  puts "Would you like to update name, template, game_id, or player_id?"
+  field = gets.chomp.downcase
+  puts "What is the updated #{field}?"
+  if field == "game_id" 
+    view_games
+  elsif field == "player_id"
+    view_players
+  end
+  correction = gets.chomp
+  db.execute("UPDATE characters SET #{field} = \"#{correction}\" WHERE id = #{id}")
 end
 
 # Driver Code / User Interface
