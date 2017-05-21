b1f05033de75d3ea04a44bfe44acda9274dc6a2e
Jae Easley <jaesley@gmail.com>
2017-05-20 17:40:58 -0700

    Remove clutter/duped code

--- databases/alt_tracker/alt_tracker.rb
+++ databases/alt_tracker/alt_tracker.rb
@@ -65,6 +65,7 @@ db.execute(cmd_create_table_characters)
 #   add to players table
 # end method
 
+
 def get_player_info(db)
   puts "Player's name:"
   name = gets.chomp
@@ -72,15 +73,6 @@ def get_player_info(db)
   location = gets.chomp
   add_player(db, name, location)
 end
-
-# method add_game
-#   get name
-#   get setting
-#   get genre
-#   get activity_status
-#     convert to boolean
-#   add to games table
-# end method
 
 def get_game_info(db)
   puts "Game's name:"
@@ -93,15 +85,6 @@ def get_game_info(db)
   activity_status = gets.chomp
   add_game(db, name, setting, genre, activity_status)
 end
-
-# method add_character
-#   get name
-#   get template
-#   show list of games
-#   get game id
-#   show list of players
-#   get player id
-# end method
 
 def get_character_info(db)
   puts "Character's name:"
@@ -129,62 +112,54 @@ def add_character(db, name, template, game_id, player_id)
   db.execute("INSERT INTO characters (name, template, game_id, player_id) VALUES (?, ?, ?, ?)", [name, template, game_id, player_id])
 end
 
-# method add_player
-#   get name
-#   get location
-#   add to players table
-# end method
+# to view db rows:
+# store view options as hash key, sql query as VALUES
+#   index as key, value is array of option + sql query?
+#     will have to make individual cmds for each output
+#     array should be printed text + cmd name
+#     then can run cmd via array index
+# select the number to retrieve the value for that key
 
-def get_player_info(db)
-  puts "Player's name:"
-  name = gets.chomp
-  puts "Player's location (state if American, country otherwise):"
-  location = gets.chomp
-  add_player(db, name, location)
+def view_options(db)
+  options = {}
+  options[1] = ["View all players.", "view_players(db)"]
+  # options[2] = ["View all games.", view_games(db)]
+  # options[3] = ["View all characters.", view_characters(db)]
+  # options[5] = ["View all active games.", view_games_active(db)]
+  # options[6] = ["View all games in a genre", view_games_genre(db)]
+  # options[7] = ["View all characters played by a player.", view_characters_player(db)]
+  # options[8] = ["View all characters on a game.", view_characters_game(db)]
+  # options[9] = ["View all characters played by a player on a game.", view_characters_player_game(db)]
+
+  options.each do | index, option |
+    puts "#{index}. #{option[0]}"
+  end
+
+  view = gets.chomp.to_i
+  options[view][1]
+  # It runs the cmd text when adding to hash. How to make it add it as text
+  # and then run the command by calling it later by hash?
 end
 
-# method add_game
-#   get name
-#   get setting
-#   get genre
-#   get activity_status
-#     convert to boolean
-#   add to games table
-# end method
-
-def get_game_info(db)
-  puts "Game's name:"
-  name = gets.chomp
-  puts "Game's setting:"
-  setting = gets.chomp
-  puts "Game's genre:"
-  genre = gets.chomp
-  puts "Game's current activity status (true/false):"
-  activity_status = gets.chomp
-  add_game(db, name, setting, genre, activity_status)
+def view_players(db)
+  players = db.execute("SELECT * FROM players")
+  players.each do |player|
+    puts "#{player['id']}\t#{player['name']}\t\t#{player['location']}"
+  end
 end
 
-# method add_character
-#   get name
-#   get template
-#   show list of games
-#   get game id
-#   show list of players
-#   get player id
-# end method
+def view_games(db)
+  games = db.execute("SELECT * FROM games")
+  games.each do |game|
+    puts "#{game['id']}\t#{game['name']}\t#{game['setting']}\t#{game['genre']}\t#{game['activity_status']}"
+  end
+end
 
-def get_character_info(db)
-  puts "Character's name:"
-  name = gets.chomp
-  puts "Character's template:"
-  template = gets.chomp
-  puts "Game's ID number:"
-  view_games(db)
-  game_id = gets.chomp
-  puts "Player's ID number:"
-  view_players(db)
-  player_id = gets.chomp
-  add_character(db, name, template, game_id, player_id)
+def view_characters(db)
+  characters = db.execute("SELECT characters.id, characters.name, characters.template, games.name, players.name FROM characters, games, players WHERE games.id = characters.game_id AND players.id = characters.player_id;")
+  characters.each do |character|
+    puts "#{character[0]}\t#{character[1]}\t#{character[2]}\t#{character[3]}\t#{character[4]}"
+  end
 end
 
 # Driver Code / User Interface
