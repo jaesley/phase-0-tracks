47e41099eaf330763777decfb3316cca7cd92a0e
Jae Easley <jaesley@gmail.com>
2017-05-26 07:30:42 -0700

    Add code for all options

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -4,12 +4,61 @@ $(document).ready(
     $('#recommendation-head').hide();
 
     $('#bermuda').click(
-    function() {
-      $('#recommendation-head').show();
-      $('#bermuda-info').show();
-    }
-  );
+      function() {
+        $('#recommendation-head').show();
+        $('#bermuda-info').show();
+      }
+    );
 
+    $('#seattle').click(
+      function() {
+        $('#hidden-recommendation').hide();
+        $('#recommendation-head').show();
+        $('#seattle-info').show();
+      }
+    );
+    
+    $('#alaska').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#alaska-info').show();
+      }
+    );
+    
+    $('#florida').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#florida-info').show();
+      }
+    );
+    
+    $('#hawaii').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#hawaii-info').show();
+      }
+    );
+    
+    $('#newyork').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#newyork-info').show();
+      }
+    );
+    
+    $('#ireland').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#ireland-info').show();
+      }
+    );
+    
+    $('#canada').click(
+      function() {
+        $('#recommendation-head').show();
+        $('#canada-info').show();
+      }
+    );
   }
 );
 
